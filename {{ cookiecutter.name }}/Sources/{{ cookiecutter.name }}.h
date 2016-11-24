5c6f9c3fbe467dda8812a84bb7c2109bf6659ec6
Rahul Katariya <rahulkatariya@me.com>
2016-11-24 11:05:46 +0530

    Replace UIKit with Foundation
    UIKit is only available in iOS and It won't compile for other platforms.

--- {{ cookiecutter.name }}/Sources/{{ cookiecutter.name }}.h
+++ {{ cookiecutter.name }}/Sources/{{ cookiecutter.name }}.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 {{ cookiecutter.organization_name }}. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for {{ cookiecutter.name }}.
 FOUNDATION_EXPORT double {{ cookiecutter.name }}VersionNumber;
